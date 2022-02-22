85e4c01deb044bca33fecdfe9b7d3d45cc6730a8
ToastersUnited <81653971+NILL2021@users.noreply.github.com>
2022-02-22 16:09:23 +0000

    Create ir-IR.js
    Created Irish translations. These aren't direct translations, and had to be changed slightly, as some English words are not in Irish.

---
+++ lang/ir-IR.js
@@ -0,0 +1,92 @@
+export default {
+    global: {
+        close: 'Dún',
+        discard: 'Caill athruithe',
+        save: 'Bhailiú',
+        saveAndClose: 'Shábháil agus dhún',
+        theme: 'Bhail nDathanna',
+        language: 'Teanga',
+    },
+    countdown: {
+        day: 'Laethanta | Lá',
+        hour: 'Uaireachta | Uair',
+        minute: 'Noiméad | Noiméad',
+        second: 'Siocind | Siocind',
+    },
+    navigation: {
+        home: 'Teacht',
+        about: 'Faoi Itinerary',
+        explore: 'Féach',
+        developers: 'Cruthaitheoirí',
+        feedback: 'Feedback',
+        create: 'Nua',
+        logIn: 'Tar isteach',
+        logOut: 'Tar amach',
+    },
+    welcomeBanner: {
+        gameJamSlogan: 'Faigh agus tar isteach jams cluiche ó Scratch',
+        communitySlogan: 'Ceiliúradh nGrúpa',
+        start: 'Tús',
+        moreInfo: 'Foghlaim mór',
+    },
+    footer: {
+        aboutTheSite: 'Foghlaim faoi Itinterary',
+        forDevelopers: 'Cruthaitheoirí',
+        affiliation: 'Ni dTáim Itinerary faoi Scratch, an Grúpa Scratch, no an Foundation Scratch.',
+        projectNature: 'Tá Itinerary an oscail faoinse tionscadal, le dáth gné ó <b>Ocular</b> (by <b>@Jeffalo</b>) agus an <b>Scratch website</b> (by the <b>Scratch Foundation</b>).',
+    },
+    notifications: {
+        global: {
+            fillInRequiredFields: 'Déan an spásana seo!',
+            invalidURL: 'Ní raibh an URL seo ceart.'
+        },
+        admin: {
+            editingOwnProfile: 'Tá tú ag athrú an profile do; bígi curmach nuair ag athrú rudaí'
+        },
+        auth: {
+            loggedOut: 'Chuaigh tú amach i gceart.',
+            failedAuth: 'Ní FluffyScratch auth ceart! ní raibh an corás tar isteach le haghaidh New Scratchers.',
+            serverError: "Ní raibh ár córas ag leabhair le FluffyScratch agus/no an API Scratch. trial arís, le do thoil",
+            banned: 'Tá tú ag toirmeasc ó Itinerary! Tá tú ag úsáid an corás ag tháinigh amach.',
+        },
+        gameJams: {
+            projects: {
+                logInToUpvoteProject: 'Tar isteach to Ceiliúradh rudaí.',
+                removingProject: 'Marú don obair...',
+                removedProject: 'Bhí don obair ag marú ó game jam seo!',
+                couldNotRemoveProject: "Ní raibh don obair ag marú ó game jam seo!",
+                selectingAsWinner: 'Déanann an buaiteoir...',
+                selectedAsWinner: "Hooray, faighimid an buaiteoir!",
+                removingWinnerStatus: 'Marú ainm buaiteoir...',
+                removedWinnerStatus: 'Maraíodh an ainm buateoir ó jam.',
+                submittingProject: 'Déanann do rudaí...',
+                submittedProject: 'Rinne do rudaí!'
+            },
+            new: {
+                creatingGameJam: 'Déanann do jam...',
+                createdGameJam: 'Rinne do jam!'
+            },
+            managers: {
+                givingUpManagerRole: "Marú tú ó liosta de cruthaitheoirí...",
+                gaveUpManagerRole: 'Ní tháim tú cruthaitheoirí de jam seo.',
+                couldNotGiveUpManagerRole: "Níorbh fhéidir linn marú don ainm cruthaitheoirí!",
+                removingManager: 'Marú cruthaitheoirí...',
+                removedManager: "Marú {manager}'s ainm cruthaitheoirí le haghaidh jam seo.",
+                couldNotRemoveManager: "Níorbh fhéidir linn marú {manager}'s ainm cruthaitheoirí le haghaidh jam seo!",
+                addingManager: 'Ag déanamh cruthaitheoir...',
+                addedManager: 'Rinne {manager} an cruthaitheoir de jam seo!',
+            },
+            updatingGameJam: 'Nuashonrú do jam...',
+            updatedGameJam: 'Bhí an jam nuashonrú i gceart!',
+            deletingGameJam: 'Ag marú do jam...',
+            deletedGameJam: 'Tá do jam ag maraíodh i dceart!',
+            couldNotDeleteGameJam: "Ní fhéadfadh do jam ag marú!",
+            savingAdvancedOptions: 'Ag shábháil roghanna speisialta...',
+            savedAdvancedOptions: 'Shábháil roghanna speisialta!',
+            featuringGameJam: 'Featuring an jam seo...',
+            featuredGameJam: 'Tá an jam seo featured, agus bíonn sé i leathanach tosaigh!',
+            unfeaturingGameJam: 'Unfeaturing jam seo...',
+            unfeaturedGameJam: 'Ní raibh an jam seo i featured.'
+        },
+    },
+};
